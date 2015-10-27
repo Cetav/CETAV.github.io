8aa5f6ca447b103a3b92f0dd3aa9bae5661464eb
LauGon29 <lauragoncode29@gmail.com>
2015-10-27 07:32:04 -0600

    se cambiar el search y se utiliza typist.js para la animación de la frase del header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-// sticky menu
-
+/*
+  *** Sticky menu ***
+*/ 
 (function () {
+  var searcher = document.getElementById("searcher");
+  var searcherInput = document.getElementById("searcherInput");
+  
+
   var targetElement = document.querySelector('.top-header'),
       scrollTop,
       targetOffsetTop = targetElement.offsetTop;
@@ -14,4 +19,49 @@
       targetElement.classList.remove('fixed');
     }
   }, false);
+
+
+  function searchExtended(){
+    searcherInput.style.display = "none";
+    searcher.onclick = function(){
+      if(searcherInput.style.display == 'none'){
+        searcherInput.style.display = 'inline-block';
+      }
+      else {
+        searcherInput.style.display = 'none';
+      }
+    }  
+  }searchExtended();
+
 }());
+
+
+/*
+  *** Typing Text Header ***
+*/ 
+$(function(){
+
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
+
+
